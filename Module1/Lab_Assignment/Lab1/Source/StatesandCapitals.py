9c499c4b4d83cef9b6038effd0dcbf00a5b12a52
Charan Kottapalli <kottapallicharan@gmail.com>
2019-02-13 22:13:50 -0600

    code updated

---
+++ Module1/Lab_Assignment/Lab1/Source/StatesandCapitals.py
@@ -0,0 +1,22 @@
+import requests
+from bs4 import BeautifulSoup
+
+def collect_href():
+    url = "https://en.wikipedia.org/wiki/List_of_state_and_union_territory_capitals_in_India"
+
+    source_code = requests.get(url)
+    source_text = source_code.text
+    soup_text = BeautifulSoup(source_text, 'html.parser')
+    #print(soup_text)
+    for link in soup_text.findAll('a'):
+        href = link.get('href')
+
+    table = soup_text.find("table",{"class":"wikitable sortable plainrowheaders"})
+
+    for rows in table.findAll('td'):
+        print(rows.text)
+
+
+
+if __name__ == '__main__':
+    collect_href()

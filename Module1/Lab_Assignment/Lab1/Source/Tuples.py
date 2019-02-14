bc5a2a5bb0b1f465c8023a5e77c44716c138331c
Ruthvicp <ruthvic7@gmail.com>
2019-02-13 22:05:02 -0600

    Updated tuples, dictionary problem

--- Module1/Lab_Assignment/Lab1/Source/Tuples.py
+++ Module1/Lab_Assignment/Lab1/Source/Tuples.py
@@ -1,2 +1,27 @@
-a = [‘John’,(‘Physics’, 80)]
-print(a)
+"""
+@author : ruthvicp
+date : 2/13/2019
+"""
+# function to convert tuples to dictionary
+def tup_to_dict(tup, dict):
+    for a, b in tup:
+        dict.setdefault(a, []).append(b)
+    return dict
+
+# function to sort dictionary whose values are list of tuples
+def sort_dict(dict):
+    for idx,list_of_tups in dict.items():
+        # key - idx, value = list_of_tups
+        dict[idx] = sorted(list_of_tups,key=lambda x: x[1]) # sorts on 1st value of list
+    return dict
+
+tup = [( 'John', ('Physics', 80)) , ('Daniel', ('Science', 90)), ('John', ('Science', 95)),
+       ('Mark',('Maths', 100)), ('Daniel', ('History', 75)), ('Mark', ('Social', 95))]
+
+# empty dictionary
+dict = {}
+dict = tup_to_dict(tup,dict)
+print("Output before sorting is : ")
+print(dict)
+print("Output after sorting is : ")
+print(sort_dict(dict))
